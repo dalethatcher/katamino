e809f2d84affb72878a97023dd2416b69abe2110
Dale Thatcher <dale.thatcher@gmail.com>
2023-12-24 17:40:26 +0100

    Add horizontal flip function

--- src/pieces.rs
+++ src/pieces.rs
@@ -28,9 +28,23 @@ fn shape_from_template(template: Vec<&str>) -> Piece {
     Piece { height, width, shape }
 }
 
+fn flip_horizontaly(piece: &Piece) -> Piece {
+    let mut shape: Vec<bool> = Vec::new();
+
+    for r in 0..piece.height {
+        for c in 0..piece.width {
+            let copy_index = usize::from(r * piece.width + (piece.width - c - 1));
+
+            shape.push(piece.shape[copy_index]);
+        }
+    }
+
+    Piece { height: piece.height, width: piece.width, shape }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::pieces::shape_from_template;
+    use crate::pieces::{flip_horizontaly, shape_from_template};
 
     #[test]
     fn can_create_shape_from_template() {
@@ -40,6 +54,20 @@ mod tests {
 
         assert_eq!(2, piece.height);
         assert_eq!(3, piece.width);
-        assert_eq!(vec![true, false, true, true, true, true], piece.shape)
+        assert_eq!(vec![true, false, true, true, true, true], piece.shape);
+    }
+
+    #[test]
+    fn can_flip_piece_horizontally() {
+        let input = shape_from_template(vec![
+            "*..",
+            "***",
+        ]);
+
+        let flipped = flip_horizontaly(&input);
+
+        assert_eq!(2, flipped.height);
+        assert_eq!(3, flipped.width);
+        assert_eq!(vec![false, false, true, true, true, true], flipped.shape);
     }
 }

e11f872c2842fec2e578cb0b0c25a627e49dbdc6
Dale Thatcher <dale.thatcher@gmail.com>
2023-12-26 23:02:17 +0100

    Display solved puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use std::time::Instant;
 use crate::board::{Board, Placement};
 use crate::pieces::{Piece, shape_from_template};
 
 mod pieces;
 mod board;
+
+fn print_state(board: &Board) {
+    for row in board.index_grid() {
+        print!("|");
+        for index in row {
+            if index == -1 {
+                print!(" ");
+            } else {
+                print!("{:x}", index);
+            }
+        }
+        println!("|");
+    }
+}
 
 fn place_pieces(board: &mut Board, remaining: &[Vec<Piece>]) -> bool {
     for transform in &remaining[0] {
@@ -17,8 +32,7 @@ fn place_pieces(board: &mut Board, remaining: &[Vec<Piece>]) -> bool {
                 if board.try_add(placement) {
                     if remaining.len() == 1 || place_pieces(board, &remaining[1..]) {
                         return true;
-                    }
-                    else {
+                    } else {
                         board.remove_last();
                     }
                 }
@@ -31,29 +45,55 @@ fn place_pieces(board: &mut Board, remaining: &[Vec<Piece>]) -> bool {
 
 fn main() {
     let shapes: Vec<Vec<Piece>> = vec![
-        shape_from_template(vec![
-            "*..",
-            "***",
-        ]),
-        shape_from_template(vec!["**"]),
+        // shape_from_template(vec![".*.", "***", ".*."]),       // 12
+        // shape_from_template(vec!["*****"]),             //  1
+        shape_from_template(vec!["*...", "****"]),      //  2
+        shape_from_template(vec![".*..", "****"]),      //  3
+        shape_from_template(vec!["**..", ".***"]),      //  4
+        shape_from_template(vec!["*..", "*..", "***"]), //  5
+        shape_from_template(vec!["***", "**."]),        //  6
+        // shape_from_template(vec!["*.*", "***"]),        //  7
+        shape_from_template(vec!["*..", "***", "..*"]), //  8
+        // shape_from_template(vec!["*..", "***", ".*."]), //  9
+        shape_from_template(vec!["***", ".*.", ".*."]), // 10
+        shape_from_template(vec!["**.", ".**", "..*"]), // 11
     ].iter()
         .map(Piece::all_transforms)
         .collect();
     let mut board = Board {
-        width: 3,
-        height: 2,
+        width: 8,
+        height: 5,
         placements: vec![],
     };
 
+    let start = Instant::now();
     if place_pieces(&mut board, shapes.as_slice()) {
-        println!("found solution!");
-        for row in board.index_grid() {
-            for index in row {
-                print!("{}", index);
-            }
-            println!();
-        }
+        let elapsed = start.elapsed();
+        println!("found solution in {}ms!", elapsed.as_millis());
+        print_state(&board);
+
     } else {
         println!("no solution found :(");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{place_pieces, print_state};
+    use crate::board::Board;
+    use crate::pieces::{Piece, shape_from_template};
+
+    #[test]
+    fn can_place_pieces() {
+        let pieces: Vec<Vec<Piece>> = vec![
+            shape_from_template(vec!["*.*", "***"]),
+            shape_from_template(vec!["*.*", "***"]),
+            shape_from_template(vec![".*.", "***", ".*."]),
+        ].iter().map(Piece::all_transforms).collect();
+        let mut board = Board { width: 5, height: 3, placements: vec![] };
+
+        assert!(place_pieces(&mut board, pieces.as_slice()));
+        print_state(&board);
+        assert!(true)
     }
 }
